2969c3724cdbcde589e69ed7191200301cc96827
Данил Володин <volodin_dv@atb.su>
2023-01-23 01:08:12 +0600

    add some commands

---
+++ build/commands.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.commands = void 0;
+exports.commands = `
+/start - Запуск бота
+/help - Посмотреть все команды
+`;
--- build/index.js
+++ build/index.js
@@ -1,10 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const telegraf_1 = require("telegraf");
+const commands_1 = require("./commands");
 require('dotenv').config();
 const bot = new telegraf_1.Telegraf(process === null || process === void 0 ? void 0 : process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.start((ctx) => { var _a; return ctx.reply(`Hi ${(_a = ctx.message.from.first_name) !== null && _a !== void 0 ? _a : 'stranger'}`); });
+bot.help((ctx) => ctx.reply(commands_1.commands));
 bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
