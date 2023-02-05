4f0d94971c29bce269ceacd5f7f6581e1d43f317
Данил Володин <volodin_dv@atb.su>
2023-02-05 10:01:25 +0600

    switched to another telegram library

--- build/commands.js
+++ build/commands.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.commands = void 0;
-exports.commands = `
+const index_1 = require("./index");
+const commandsHelp = `
 /start - Запуск бота
 /help - Посмотреть все команды
 `;
+const botCommands = [
+    { command: '/start', description: 'Начальное приветствие' },
+    { command: '/info', description: 'Получить информацию' }
+];
+index_1.bot.setMyCommands(botCommands);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,44 @@
-import {Telegraf} from "telegraf";
-import {commands} from "./commands";
+import {Message} from "node-telegram-bot-api";
+
+const TelegramBot = require('node-telegram-bot-api');
+const fetch = require('node-fetch')
 require('dotenv').config()
 
-const bot = new Telegraf(process?.env.BOT_TOKEN!);
-bot.start((ctx) => ctx.reply(`Hi ${ctx.message.from.first_name ?? 'stranger'}`));
-bot.help((ctx) => ctx.reply(commands));
+const token = process.env['BOT_TOKEN']
+export const bot = new TelegramBot(token, {polling: true});
 
-bot.launch();
+const start = () => {
+    bot.on('message', async (msg: Message) => {
+        const chatId = msg.chat.id;
+        const text = msg.text
+        if (text === '/start') {
+            return await bot.sendMessage(chatId,'Ниже появится кнопка для призыва котика', {
+                reply_markup: {
+                    keyboard: [
+                        [{text: 'Хочу котика', }]
+                    ],
+                    resize_keyboard: true
+                }
+            })
+        }
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.from?.first_name} ${msg.from?.last_name}`)
+        }
+        if (text === 'Хочу котика'){
+            bot.sendMessage(chatId, 'Вот котик')
+            return await fetch('https://api.thecatapi.com/v1/images/search')
+                .then(res => res.json())
+                .then(responceBody => {
+                    console.log(responceBody)
+                    return bot.sendPhoto(chatId, responceBody[0].url)
+                })
+                .catch((e: any) => {
+                    console.log(e)
+                })
+        }
+        return bot.sendMessage(chatId, 'Я тебя не понимаю')
+    });
+}
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+start()
